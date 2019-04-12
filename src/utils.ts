857c2dddd9f8b631cdeac19792775443e5995172
huguang <guangyu9@staff.sina.com.cn>
2019-04-12 19:16:55 +0800

    Update curryRight2 function.

--- src/utils.ts
+++ src/utils.ts
@@ -1,14 +1,21 @@
 /**
- *
+ * Transform function into a curried function with 2 parameters.
  *
  * @export
  * @param {Function} func
  * @returns
  */
-export function curryRight2(func: Function) {
-  return function(arg2: any) {
-    return function(arg1: any) {
-      return func(arg1, arg2);
-    };
-  };
-}
+export const curryRight2 = <T, U, V>(func: (arg1: T, arg2: U) => V) => (
+  arg2: U
+) => (arg1: T): V => func(arg1, arg2);
+
+/**
+ * Transform function into a curried function with 3 parameters.
+ *
+ * @export
+ * @param {Function} func
+ * @returns
+ */
+export const curryRight3 = <T, U, V, W>(
+  func: (arg1: T, arg2: U, arg3: V) => W
+) => (arg3: V) => (arg2: U) => (arg1: T): W => func(arg1, arg2, arg3);
